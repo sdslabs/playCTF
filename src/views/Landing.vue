59e2447bcb71d18262885ea3ea3f6d9ff5e8c287
Aviral Jain <avi.aviral140@gmail.com>
2021-02-06 18:13:53 +0530

    Fix css and redirection for landing page

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -2,29 +2,35 @@
   <div class="home">
     <div class="event">
       <div class="event-info">
-        <div class="tagline">Be a part of noobCTF 2019</div>
-        <div class="description">
-          noobCTF is a free computer security game targeted at middle and high
-          school students, created by security experts at SDSLabs.
+        <div class="event-info-text">
+          <div class="tagline">Be a part of noobCTF 2019</div>
+          <div class="description">
+            noobCTF is a free computer security game targeted at middle and high
+            school students, created by security experts at SDSLabs.
+          </div>
+          <div class="timing">4th December 2019, Sunday</div>
+          <router-link to="/challenges"
+            ><Button text="See Challenges" class="primary-btn"
+          /></router-link>
         </div>
-        <div class="timing">4th December 2019, Sunday</div>
-        <router-link to="/register"><Button text="Register now"/></router-link>
+        <div class="event-info-img">
+          <img class="landing-img" src="@/assets/landing1.svg" />
+        </div>
       </div>
       <div class="prize-info">
-        <div class="heading">Prizes to be won</div>
-        <div class="description">
-          The real gem to acquire is the experience time bound competition
-          provides. However, Participants from the top three winning teams get
-          to appear directly for SDSLabs interview round.
+        <div class="prize-info-text">
+          <div class="heading">Prizes to be won</div>
+          <div class="description">
+            The real gem to acquire is the experience time bound competition
+            provides. However, Participants from the top three winning teams get
+            to appear directly for SDSLabs interview round.
+          </div>
         </div>
-        <div class="learn-more-dropdown">
-          Learn more
-          <img src="@/assets/dropdown.svg" />
+        <div class="prize-info-img">
+          <img class="landing-img" src="@/assets/landing2.svg" />
         </div>
       </div>
     </div>
-    <img class="img1" src="@/assets/landing1.svg" />
-    <img class="img2" src="@/assets/landing2.svg" />
   </div>
 </template>
 
@@ -33,11 +39,11 @@ import Button from "@/components/Button.vue";
 export default {
   name: "home",
   components: {
-    Button
+    Button,
   },
   beforeCreate() {
     this.$store.commit("updateCurrentPage", "AboutCTF");
-  }
+  },
 };
 </script>
 
