fa4aa6ac926b4747e61e45553eb10290b5fa108a
Aviral Jain <avi.aviral140@gmail.com>
2021-02-11 14:35:46 +0530

    make and reuse the admin dropdown component

---
+++ src/components/AdminDropdown.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="adminNavDropdown">
+    <button class="adminNavDropbtn">
+      Admin
+      <img :src="dropdown" />
+    </button>
+    <div class="adminNavDropdownContent">
+      <a class="adminNavDropdownLink" href="#">Link 1</a>
+      <a class="adminNavDropdownLink" href="#">Link 2</a>
+      <a class="adminNavDropdownLink" href="#">Link 3</a>
+    </div>
+  </div>
+</template>
+
+<script>
+import { dropdown } from "../constants/images";
+export default {
+  name: "AdminDropdown",
+  data() {
+    return {
+      dropdown
+    };
+  }
+};
+</script>
--- src/components/AdminLandingHeader.vue
+++ src/components/AdminLandingHeader.vue
@@ -1,27 +1,14 @@
 <template>
   <div class="adminLandingHeader">
-    <div class="adminNavDropdown">
-      <button class="adminNavDropbtn">
-        Admin
-        <img :src="dropdown" />
-      </button>
-      <div class="adminNavDropdownContent">
-        <a class="adminNavDropdownLink" href="#">Link 1</a>
-        <a class="adminNavDropdownLink" href="#">Link 2</a>
-        <a class="adminNavDropdownLink" href="#">Link 3</a>
-      </div>
-    </div>
+    <AdminDropdown />
   </div>
 </template>
 <script>
-import { dropdown } from "../constants/images";
+import AdminDropdown from "@/components/AdminDropdown";
 export default {
   name: "AdminLandingHeader",
-  components: {},
-  data() {
-    return {
-      dropdown
-    };
+  components: {
+    AdminDropdown
   }
 };
 </script>
