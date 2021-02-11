4cb6f95fd7572614a5b86f8c932f99d85c788534
Aviral Jain <avi.aviral140@gmail.com>
2021-02-11 17:03:41 +0530

    add progress bar and action buttons for landing page

--- src/views/AdminLanding.vue
+++ src/views/AdminLanding.vue
@@ -4,5 +4,52 @@
       <span class="orange"> WELCOME! </span>
       <span>LET'S START BUILDING YOUR COMPETITION</span>
     </div>
+    <AdminLandingProgress
+      :totalSteps="totalSteps"
+      :currentStep="getCurrentStep()"
+    />
+    <div v-if="totalSteps === getCurrentStep()" class="action">
+      <button v-if="getCurrentStep() !== 1" class="primary-btn" @click="goBack">
+        Back
+      </button>
+      <button class="primary-btn action-btn" @click="submitConfig">
+        Proceed
+      </button>
+    </div>
+    <div v-else class="action">
+      <button v-if="getCurrentStep() !== 1" class="primary-btn" @click="goBack">
+        Back
+      </button>
+      <button class="primary-btn action-btn" @click="goNext">Next</button>
+    </div>
   </div>
 </template>
+
+<script>
+import AdminLandingProgress from "../components/AdminLandingProgress";
+export default {
+  data() {
+    return {
+      currentStep: 1,
+      totalSteps: 5,
+    };
+  },
+  components: {
+    AdminLandingProgress,
+  },
+  methods: {
+    getCurrentStep() {
+      return this.currentStep;
+    },
+    goNext() {
+      this.currentStep++;
+    },
+    goBack() {
+      if (this.currentStep > 1) {
+        this.currentStep--;
+      }
+    },
+    submitConfig() {},
+  },
+};
+</script>
