2533eefbf80d3a02a498e9d7c7d48721fd7d0879
shubhamgupta2956 <shubham.gupta2956@gmail.com>
2021-02-21 06:01:08 +0530

    Fix saving of token and linting fix in some files

--- src/store/index.js
+++ src/store/index.js
@@ -6,8 +6,8 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   plugins: [
     createPersistedState({
-      storage: window.sessionStorage
-    })
+      storage: window.sessionStorage,
+    }),
   ],
   state: {
     currentPage: "",
@@ -17,25 +17,25 @@ export default new Vuex.Store({
       token: null,
       login: false,
       access: false,
-      role: null
+      role: null,
     },
     competitionInfo: {
       competitionName: "",
       communicationPlatformLink: "",
       startingTime: "23:07:05 UTC: +06:00, 9th February 2021, Tuesday",
       endingTime: "07:07:00 UTC: +06:00, 18th February 2021, Thursday",
-      logo: ""
+      logo: "",
     },
     hostUrl: "http://localhost:5005",
-    challengeHostUrl: "http://challenges.beast.com"
+    challengeHostUrl: "http://challenges.beast.com",
   },
   mutations: {
     updateCurrentPage(state, page) {
       state.currentPage = page;
     },
-    async updateUserAuth(state, username, key, role) {
+    updateUserAuth(state, { username, token, role }) {
       state.userInfo.userName = username;
-      state.userInfo.token = key;
+      state.userInfo.token = token;
       state.userInfo.login = true;
       state.userInfo.role = role;
       state.userInfo.access = true;
@@ -47,27 +47,27 @@ export default new Vuex.Store({
       state.userInfo.access = false;
       state.userInfo.role = null;
       sessionStorage.clear();
-    }
+    },
   },
   getters: {
-    login: state => state.userInfo.login,
-    currentPage: state => state.currentPage,
-    hostUrl: state => state.hostUrl,
-    challengeHostUrl: state => state.challengeHostUrl,
-    getUsername: state => {
+    login: (state) => state.userInfo.login,
+    currentPage: (state) => state.currentPage,
+    hostUrl: (state) => state.hostUrl,
+    challengeHostUrl: (state) => state.challengeHostUrl,
+    getUsername: (state) => {
       return state.userInfo.userName;
     },
-    getToken: state => {
+    getToken: (state) => {
       return state.userInfo.token;
     },
-    getState: state => {
+    getState: (state) => {
       return state.userInfo.login;
     },
-    getAccess: state => {
+    getAccess: (state) => {
       return state.userInfo.access;
     },
-    getRole: state => {
+    getRole: (state) => {
       return state.userInfo.role;
-    }
-  }
+    },
+  },
 });
