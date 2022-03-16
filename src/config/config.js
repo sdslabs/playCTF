1795898008794e310ccb1b5b9c06ed29a03ff8b4
burnerlee <avi.aviral140@gmail.com>
2022-03-17 02:18:36 +0530

    minor fixes

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
-  beastRoot: "http://localhost:5005",
-  staticRoot: "http://static.scythe2021.sdslabs.co/",
-  webRoot: "http://hack.scythe2021.sdslabs.co",
-  ncRoot: "hack.scythe2021.sdslabs.co"
+  beastRoot: "http://testctf.sdslabs.co/server/",
+  staticRoot: "http://static.testctf.sdslabs.co/static/",
+  webRoot: "http://hack.testctf.sdslabs.co",
+  ncRoot: "hack.testctf.sdslabs.co"
 };
