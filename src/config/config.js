5d9db9823884a3f9f698681376dc80bee7fdb84f
Ubuntu <sdslabs@Beast-prod.3a1ktsmy5tvuzenlrx03dqoyvc.rx.internal.cloudapp.net>
2022-03-17 19:02:03 +0530

    update static url to https

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
   beastRoot: "https://noobctf.sdslabs.co/server",
-  staticRoot: "http://static2.noobctf.sdslabs.co/static/",
+  staticRoot: "https://static2.noobctf.sdslabs.co/static/",
   webRoot: "http://hack.noobctf.sdslabs.co",
   ncRoot: "hack.noobctf.sdslabs.co"
 };
