4f6481344ade811ad5b855d38c4a6f35d0421979
Aviii06 <architgosain0601@gmail.com>
2022-06-27 23:04:40 +0530

    add spaces and button for logs in configure tab

--- src/views/AdminConfigure.vue
+++ src/views/AdminConfigure.vue
@@ -2,15 +2,20 @@
   <div class="adminConfigureContainer">
     <div class="heading">
       <span class="headingText"> CONFIGURE</span>
+      <div class="configureButtons">
+        <button class="action-cta" @click="showPreviewModal = true">
+          <img :src="preview" />
+          <span>Preview</span>
+        </button>
 
-      <button class="action-cta" @click="showPreviewModal = true">
-        <img :src="preview" />
-        <span>Preview</span>
-      </button>
+        <button class="action-cta" @click="sendRestartRequest">
+          <span>Restart</span>
+        </button>
 
-      <button class="action-cta" @click="sendRestartRequest">
-        <span>Restart</span>
-      </button>
+        <button class="action-cta" onclick="location.href = '/logs';">
+          <span>Logs</span>
+        </button>
+      </div>
 
       <div class="addConfigFeedback">
         <ErrorBox v-if="msg" :msg="msg" :icon="icon" />
@@ -36,7 +41,7 @@
           startTime: compStartTime,
           startDate: compStartDate,
           endDate: compEndDate,
-          endTime: compEndTime
+          endTime: compEndTime,
         }"
         :disabled="true"
       ></ConfigTimeDate>
@@ -80,7 +85,7 @@ export default {
     ConfigTimeDate,
     ConfigLogo,
     PreviewModal,
-    ErrorBox
+    ErrorBox,
   },
   data() {
     return {
@@ -100,7 +105,7 @@ export default {
       compLogo: "",
       showPreviewModal: false,
       msg: null,
-      icon: null
+      icon: null,
     };
   },
   methods: {
@@ -133,7 +138,7 @@ export default {
       return value;
     },
     sleep(ms) {
-      return new Promise(resolve => setTimeout(resolve, ms));
+      return new Promise((resolve) => setTimeout(resolve, ms));
     },
     async fadeFunc() {
       await this.sleep(3000);
@@ -142,7 +147,7 @@ export default {
     },
     updateConfigs() {
       let timezone = moment.tz.names()[
-        getAllTimezones().findIndex(el => {
+        getAllTimezones().findIndex((el) => {
           return el === this.compTimezone;
         })
       ];
@@ -165,7 +170,7 @@ export default {
         startingTime,
         endingTime,
         timezone: this.compTimezone,
-        logo: this.compLogo
+        logo: this.compLogo,
       };
       configureService
         .updateConfigs(configs)
@@ -177,14 +182,14 @@ export default {
           }
           this.$store.commit("updateCompInfo", configs);
           this.$vToastify.setSettings({
-            theme: "beast-success"
+            theme: "beast-success",
           });
           this.$vToastify.success("Changes made successfully", "Success");
           this.fadeFunc();
         })
         .catch(() => {
           this.$vToastify.setSettings({
-            theme: "beast-error"
+            theme: "beast-error",
           });
           this.$vToastify.error("Failed to make changes", "Error");
           this.fadeFunc();
@@ -197,16 +202,16 @@ export default {
       this.showPreviewModal = false;
     },
     sendRestartRequest() {
-      fetch('/restart/');
+      fetch("/restart/");
       this.$vToastify.setSettings({
-        theme: "beast-success"
+        theme: "beast-success",
       });
       this.$vToastify.success("Restart requested!", "Success");
       this.fadeFunc();
-    }
+    },
   },
   mounted() {
-    configureService.getConfigs().then(response => {
+    configureService.getConfigs().then((response) => {
       let configs = response.data;
       this.compName = configs.name;
       this.compAbout = configs.about;
@@ -236,7 +241,7 @@ export default {
       if (logoName) {
         configureService
           .getLogo(`${CONFIG.beastRoot}/api/info/logo/${logoName}`, logoName)
-          .then(response => {
+          .then((response) => {
             this.compLogo = response;
           });
       } else {
@@ -246,6 +251,6 @@ export default {
   },
   beforeCreate() {
     this.$store.commit("updateCurrentPage", "adminConfigure");
-  }
+  },
 };
 </script>
