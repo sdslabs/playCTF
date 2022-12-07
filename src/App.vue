562e4b512a5beeab6d711606946711f0aa093095
reeshi-18 <reeshipaul35@gmail.com>
2022-12-07 18:32:19 +0530

    iteration2 changes

--- src/App.vue
+++ src/App.vue
@@ -32,7 +32,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $theme-color-grey-blue;
   font-size: 1vw;
   overflow: hidden;
   z-index: 1;
